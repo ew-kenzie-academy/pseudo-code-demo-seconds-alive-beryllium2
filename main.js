c6ceacb53fe160bda2b6332ce4781fe9a72bfaea
ASUSTe-UX434IQ <redhat@Stewart>
2021-06-24 12:00:47 -0600

    Its a little more readable

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-// prompt birth month, birth year
-let _MONTH_  =   prompt("In what month were you born?" );
-let _YEAR_  =    prompt("In what year were you born?"  );
-//   convert input into gregorian time
-const birthDayTime = new Date(_YEAR_,_MONTH_, 0, 0, 0, 0, 0);
-//   convert that time to seconds in an inertial frame
-  const birthEpoch= birthDayTime.valueOf()/1000;
-// get current date the same
-const currentTime = new Date();
-//   now convert that time the same
-const currentEpoch= currentTime.valueOf()/1000;
-// output the difference
-timeDifference=currentEpoch-birthEpoch;
-alert(timeDifference);
+
+
+// 1. prompt birth month and birth year
+   let _MONTH_  =   prompt("In what month were you born?" );
+   let _YEAR_  =    prompt("In what year were you born?"  );
+
+//2.  convert input into gregorian time
+//      convert that time to seconds in an inertial frame
+    // const birthDayTime = new Date(_YEAR_,_MONTH_, 0, 0, 0, 0, 0);
+    let birthDayTime = new Date(""+ _MONTH_ +" 01, "+_YEAR_+" 11:13:00");
+    let birthEpoch       = Math.floor( birthDayTime.valueOf()/1000 );
+
+//3. get current date the same
+//     now convert that time the same
+    let currentTime = new Date();
+    let currentEpoch = Math.floor( currentTime.valueOf()/1000  );
+
+//4. output the difference
+    timeDifference=currentEpoch-birthEpoch;
+    alert(timeDifference);
 
 
 /*
@@ -27,6 +33,7 @@ alert(timeDifference);
 + [[https://www.w3schools.com/js/js_dates.asp][w3schools::js_dates]] 
   *JavaScript Date Objects* 
    Perhaps there is a way to use OOP to help us in this /dynamic-typed/ language :P
+   ~const d = new Date("October 13, 2014 11:13:00");~
    #+begin_quote
    new Date()
    new Date(year, month, day, hours, minutes, seconds, milliseconds)
