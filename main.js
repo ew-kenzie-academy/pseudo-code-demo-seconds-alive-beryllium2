d69e5e33a6ac70cb4e0f4723f8e6f337830342bd
ASUSTe-UX434IQ <redhat@Stewart>
2021-06-24 11:16:07 -0600

    Thinking...

--- main.js
+++ main.js
@@ -16,7 +16,14 @@
 
 // prompt birth month
 // prompt birth year
-// convert input into gregorian time
+//   convert input into gregorian time
+//   convert that time to seconds in an inertial frame
+// get current date as gregorian time
+//   convert that time to seconds in an inertial frame
 
 
+/*
++ [[https://www.w3docs.com/snippets/javascript/how-to-get-a-timestamp-in-javascript.html][w3docs::timestamp]]
+  *Date.now () Method* =returning the number of the milliseconds, elapsed since the Unix Epoch=
 
+*/
